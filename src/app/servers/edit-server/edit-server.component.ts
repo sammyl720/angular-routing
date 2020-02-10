5a9515259ebec4f23ffe4ca1779dc94f1d98513c
sam leider <sam.leider@hotmail.com>
2020-02-09 20:39:26 -0500

    up to lecture 139

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+
+  constructor(private serversService: ServersService, private router: ActivatedRoute) { }
+
+  ngOnInit() {
+    console.log(this.router.snapshot.queryParams)
+    console.log(this.router.snapshot.fragment)
+    this.router.params.subscribe()
+    this.router.fragment.subscribe()
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}
---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService, private router: ActivatedRoute) { }
+
+  ngOnInit() {
+    const id = +this.router.snapshot.params['id']
+    this.server = this.serversService.getServer(id);
+    this.router.params.subscribe((params: Params) => {
+      this.serversService.getServer(params.id)
+    })
+  }
+
+}
